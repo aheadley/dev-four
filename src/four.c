10fda2c369bd187d80631d0af128d6ca974d7886
Alex Headley <aheadley@waysaboutstuff.com>
2011-08-10 01:23:57 -0400

    split into header file

--- src/four.c
+++ src/four.c
@@ -4,25 +4,13 @@
  * shamelessly ripped from http://tldp.org/LDP/lkmpg/2.6/html/lkmpg.html#AEN569
  */
 
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
-#include <asm/uaccess.h>
-#include <linux/init.h>
-#include <linux/device.h>
-#include <linux/types.h>
+#include "four.h"
 
 MODULE_LICENSE("GPL");
 
 /*
  * might move this stuff to a .h later
  */
-int init_module(void);
-void cleanup_module(void);
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
 #define SUCCESS     0
 #define DEVICE_NAME "four"
---
+++ src/four.h
@@ -0,0 +1,15 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include <linux/init.h>
+#include <linux/device.h>
+#include <linux/types.h>
+
+int init_module(void);
+void cleanup_module(void);
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
